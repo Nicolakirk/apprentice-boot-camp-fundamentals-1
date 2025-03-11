ec267741d17413333b92b60ba48b6a2c67a50384
Nicola Kirk <nicola.kirk@autotrader.co.uk>
2025-03-11 15:51:12 +0000

    adds playing card desk class to the cards

--- exercises/typescript/src/cards/cards.ts
+++ exercises/typescript/src/cards/cards.ts
@@ -10,60 +10,57 @@ import { suitNames } from "./suit-names";
 export class Cards {
   getCards(): string[] {
     const result: string[] = [];
-    const deck = new PlayingCardDeck;
+    const deck: PlayingCard[] = [];
 
     suitNames.forEach((suitName) => {
       for (let faceValue = 0; faceValue < 13; faceValue++) {
-        
         const newSuit = new Suit(suitName);
         const playingCard = new PlayingCard(newSuit, faceValue);
         deck.push(playingCard);
       }
-    })
-      
+    });
 
-      let cardNumber = 0;
+    let cardNumber = 0;
 
-      for (const card of deck) {
-        let faceValueName: string;
+    for (const card of deck) {
+      let faceValueName: string;
 
-        switch (card.faceValueName) {
-          case 0:
-            faceValueName = "ace";
-            break;
-          case 1:
-          case 2:
-          case 3:
-          case 4:
-          case 5:
-          case 6:
-          case 7:
-          case 8:
-          case 9:
-            faceValueName = (card.faceValueName + 1).toString();
-            break;
-          case 10:
-            faceValueName = "jack";
-            break;
-          case 11:
-            faceValueName = "queen";
-            break;
-          case 12:
-            faceValueName = "king";
-            break;
-          default:
-            throw new Error(
-              "Something went wrong " +
-                card.faceValueName +
-                " is not a valid faceValue!"
-            );
-        }
-
-        result[cardNumber] = faceValueName + " of " + card.suit.suitName;
-        cardNumber++;
+      switch (card.faceValueName) {
+        case 0:
+          faceValueName = "ace";
+          break;
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+          faceValueName = (card.faceValueName + 1).toString();
+          break;
+        case 10:
+          faceValueName = "jack";
+          break;
+        case 11:
+          faceValueName = "queen";
+          break;
+        case 12:
+          faceValueName = "king";
+          break;
+        default:
+          throw new Error(
+            "Something went wrong " +
+              card.faceValueName +
+              " is not a valid faceValue!"
+          );
       }
 
+      result[cardNumber] = faceValueName + " of " + card.suit.suitName;
+      cardNumber++;
     }
+
     return result;
   }
 }
